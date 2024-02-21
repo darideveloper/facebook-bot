261e26f2c01b9d985c1d2bc120c8e129cce02d55
darideveloper <darideveloper@gmail.com>
2024-02-21 13:18:10 -0600

    initial commit

---
+++ libs/facebook_bot.py
@@ -0,0 +1,11 @@
+from web_scraping import WebScraping
+
+
+class FacebookBot(WebScraping):
+    
+    def __init__(self):
+        print("hello")
+        
+        
+if __name__ == "__main__":
+    fb_bot = FacebookBot()
